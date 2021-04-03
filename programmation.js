d9cf39dcc34a47eac4aaa159c6c2d3df37e82cc3
Jérôme Trottier <jerometrottier@Jeromes-MacBook-Air.local>
2021-04-03 14:24:58 -0400

    bloque

--- programmation.js
+++ programmation.js
@@ -1,3 +1,4 @@
+
 function generateScale() {
 
     setScaleNote();
@@ -9,6 +10,7 @@ function setScaleNote() {
     const scaleNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
     const scaleNote = Math.ceil(Math.random() * 7) - 1;
     document.getElementById('scaleNote').innerHTML = scaleNotes[scaleNote];
+    console.log(scaleNote);
 }
 function setKeySignature() {
     const keySignatures = [' Natural', ' Flat', ' Sharp'];
@@ -21,18 +23,18 @@ function setScaleType() {
     document.getElementById('scaleType').innerHTML = scaleTypes[scaleType];
 }
 
-const htmlScale = `<div class="oneScale"><div class="noteC whiteNote"></div>
-                <div class="noteCSharp blackNote"></div>
-                <div class="noteD whiteNote"></div>
-                <div class="noteDSharp blackNote"></div>
-                <div class="noteE whiteNote"></div>
-                <div class="noteF whiteNote"></div>
-                <div class="noteFSharp blackNote"></div>
-                <div class="noteG whiteNote"></div>
-                <div class="noteGSharp blackNote"></div>
-                <div class="noteA whiteNote"></div>
-                <div class="noteASharp blackNote"></div>
-                <div class="noteB whiteNote"></div></div>`;
+const htmlScale = `<div class="oneScale"><div class="noteC whiteNote note"></div>
+                <div class="noteCSharp blackNote note"></div>
+                <div class="noteD whiteNote note"></div>
+                <div class="noteDSharp blackNote note"></div>
+                <div class="noteE whiteNote note"></div>
+                <div class="noteF whiteNote note"></div>
+                <div class="noteFSharp blackNote note"></div>
+                <div class="noteG whiteNote note"></div>
+                <div class="noteGSharp blackNote note"></div>
+                <div class="noteA whiteNote note"></div>
+                <div class="noteASharp blackNote note"></div>
+                <div class="noteB whiteNote note"></div></div>`;
 
 function drawScales() {
   const pianoElement = document.getElementById('piano');
@@ -42,5 +44,29 @@ function drawScales() {
 }
 
 window.addEventListener('load', () => {
-  drawGammes();
+  drawScales();
 })
+
+const majorScalePattern = [2, 2, 1, 2, 2, 2, 1];
+const minorScalePattern = [2, 1, 2, 2, 1, 2, 2];
+
+function highlightScales() {
+    //clearPreviousHighlights();
+    //const keys = getElementsWithNoteClass(notes);
+    getScaleNote();
+    //const getIdxFirstNote = getFirstElementIdxInScale();
+    //highlightKeys();
+
+    /*- Enlever les previous highlights
+      - Aller chercher la note de la gamme
+      - Associer cette note avec son Pattern
+      - Donenr le highlight aux bonnes touches*/
+    
+}
+function getScaleNote() {
+    const keys = document.getElementsByClassName("note");
+    console.log(keys[scaleNote]);
+}
+/*function getElementsWithNoteClass(notes) {
+    notes = document.getElementsByClassName("note");
+}*/
