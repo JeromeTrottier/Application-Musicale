af43db1319a413fc96dd974be375738a342c0f21
Samuel Trottier <samueltrottier@outlook.fr>
2021-04-03 10:17:28 -0400

    generating gammes html

--- programmation.js
+++ programmation.js
@@ -33,3 +33,27 @@ function generationGamme() {
         document.getElementById("gammeSignature").innerHTML = " Sharp"    
     }
 }
+
+const gammeHtml = `<div class="uneGamme"><div class="noteC noteBlanche"></div>
+                <div class="noteCSharp noteNoire"></div>
+                <div class="noteD noteBlanche"></div>
+                <div class="noteDSharp noteNoire"></div>
+                <div class="noteE noteBlanche"></div>
+                <div class="noteF noteBlanche"></div>
+                <div class="noteFSharp noteNoire"></div>
+                <div class="noteG noteBlanche"></div>
+                <div class="noteGSharp noteNoire"></div>
+                <div class="noteA noteBlanche"></div>
+                <div class="noteASharp noteNoire"></div>
+                <div class="noteB noteBlanche"></div></div>`;
+
+function drawGammes() {
+  const pianoElement = document.getElementById('piano');
+  for (let i = 0; i < 2; i++) {
+    pianoElement.innerHTML += gammeHtml;
+  }
+}
+
+window.addEventListener('load', () => {
+  drawGammes();
+})
